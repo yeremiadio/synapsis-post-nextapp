77d55c957c2782595ff659a8dec5b8bbe479c3db
yeremiadio <raikkonendio@gmail.com>
2024-12-26 08:47:24 +0700

    feat: add ant design config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 // These styles apply to every route in the application
 import "@/styles/globals.css";
+import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ConfigProvider theme={{ token: { fontFamily: "Inter" } }}>
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
@@ -14,6 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Synapsis Post App</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </ConfigProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,38 @@
-const Page = () => {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>;
-}
-export default Page;
+import { Button, Form, Input } from "antd";
+
+const Home = () => {
+  return (
+    <div className="justify-center items-center flex h-screen bg-slate-50">
+      <div className="bg-white px-4 py-6 rounded-lg shadow-lg w-full lg:w-[420px] mx-2">
+        <h2 className="text-gray-700 font-bold text-lg mb-1 mt-2 text-center">Synapsis Post App</h2>
+        <p className="text-gray-600 text-base mt-1 mb-2 text-center">Welcome back</p>
+        <Form layout="vertical" className="w-full">
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Access Token"
+            name="accessToken"
+            rules={[
+              { required: true, message: "Please input your access token!" },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item label={null} className="mb-0">
+            <Button className="w-full" type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+export default Home;
